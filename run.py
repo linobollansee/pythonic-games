1922ff74880a6348f69a877d2e2ed30c8d5b2a5d
Lino Bollansee <linobollansee@hotmail.com>
2024-03-14 23:53:15 +0000

    Add class WordList with words list

--- run.py
+++ run.py
@@ -6,3 +6,17 @@ from colorama import init, Fore
 
 # Initializing the colorama module for terminal color and style manipulation
 init()
+
+
+class WordList:
+    """
+    Class representing a WordList with a predefined list of words.
+    """
+
+    # Predefined list of words
+    words = [
+        'programming', 'development', 'algorithm', 'javascript', 'python',
+        'software', 'function', 'variable', 'iteration', 'recursion',
+        'database', 'framework', 'interface', 'inheritance', 'polymorphism',
+        'abstraction', 'encapsulation', 'optimization', 'debugging', 'testing'
+    ]
