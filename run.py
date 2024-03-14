07aa9cc9bb051a9c58d973553d705697444036b7
Lino Bollansee <linobollansee@hotmail.com>
2024-03-14 18:28:34 +0000

    Init colorama module in run.py

--- run.py
+++ run.py
@@ -3,3 +3,6 @@ import random
 
 # Importing 'init' and 'Fore' functions from third-party colorama module
 from colorama import init, Fore
+
+# Initializing the colorama module for terminal color and style manipulation
+init()
