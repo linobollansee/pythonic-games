00b390274c0905f03ad427fa04439f3e4dfabee1
Lino Bollansee <linobollansee@hotmail.com>
2024-03-14 18:26:31 +0000

    Add colorama module to run.py

--- run.py
+++ run.py
@@ -1,2 +1,5 @@
 # Importing standard built-in random module for generating random numbers
 import random
+
+# Importing 'init' and 'Fore' functions from third-party colorama module
+from colorama import init, Fore
